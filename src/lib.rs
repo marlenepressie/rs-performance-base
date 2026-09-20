0acec0220c752b5bceaed772481ba1896461512f
marlenepressie <marlenepressie@gmail.com>
2026-09-20 22:24:00 +0000

    chore: sync rust crate module definitions

--- src/lib.rs
+++ src/lib.rs
@@ -932,3 +932,10 @@ pub struct 10965 {
     pub id: u64,
     pub active: bool,
 }
+
+/// Telemetry node payload 1682
+#[derive(Debug, Clone)]
+pub struct 22286 {
+    pub id: u64,
+    pub active: bool,
+}
